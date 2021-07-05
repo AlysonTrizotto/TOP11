d7ff7d47ff5f88f33d1ae0c92def5115f407f2dc
AlysonTrizotto <alysontrizotto1@gmail.com>
2021-07-05 09:40:08 -0300

    Finalização do formulário
    Foi feito tratamento dos botões, foi adicionado um radio button para direcionar as formas de pagamento, ainda não foi implementado a recuperação de venda, foi implementado a edição do cadastro de pedidos para finalizar a venda, foi tratado o edit box de troco

--- Vendas Desktop/Vendas/Vendas/Telas/Fechamento.Designer.cs
+++ Vendas Desktop/Vendas/Vendas/Telas/Fechamento.Designer.cs
@@ -31,10 +31,23 @@ namespace Vendas
         {
             this.CANCELAR = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.FINALIZAR = new System.Windows.Forms.Button();
+            this.btnFinalizar = new System.Windows.Forms.Button();
             this.Mostra = new System.Windows.Forms.Label();
             this.txtFechamento = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtDinheiro = new System.Windows.Forms.TextBox();
+            this.lblDinheiro = new System.Windows.Forms.Label();
+            this.txtTroco = new System.Windows.Forms.TextBox();
+            this.lblTroco = new System.Windows.Forms.Label();
+            this.gbFormaPag = new System.Windows.Forms.GroupBox();
+            this.rbPIX = new System.Windows.Forms.RadioButton();
+            this.rbCredito = new System.Windows.Forms.RadioButton();
+            this.rbDebito = new System.Windows.Forms.RadioButton();
+            this.rbDinheiro = new System.Windows.Forms.RadioButton();
             this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.gbFormaPag.SuspendLayout();
             this.SuspendLayout();
             // 
             // CANCELAR
@@ -55,7 +68,7 @@ namespace Vendas
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.Controls.Add(this.CANCELAR, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.FINALIZAR, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnFinalizar, 2, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 350);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -64,38 +77,169 @@ namespace Vendas
             this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 100);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
-            // FINALIZAR
+            // btnFinalizar
             // 
-            this.FINALIZAR.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.FINALIZAR.Location = new System.Drawing.Point(535, 3);
-            this.FINALIZAR.Name = "FINALIZAR";
-            this.FINALIZAR.Size = new System.Drawing.Size(262, 94);
-            this.FINALIZAR.TabIndex = 1;
-            this.FINALIZAR.Text = "FINALIZAR";
-            this.FINALIZAR.UseVisualStyleBackColor = true;
-            this.FINALIZAR.Click += new System.EventHandler(this.FINALIZAR_Click);
+            this.btnFinalizar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnFinalizar.Location = new System.Drawing.Point(535, 3);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(262, 94);
+            this.btnFinalizar.TabIndex = 1;
+            this.btnFinalizar.Text = "FINALIZAR";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.Click += new System.EventHandler(this.FINALIZAR_Click);
             // 
             // Mostra
             // 
             this.Mostra.AutoSize = true;
-            this.Mostra.Location = new System.Drawing.Point(59, 68);
+            this.Mostra.Location = new System.Drawing.Point(12, 86);
             this.Mostra.Name = "Mostra";
-            this.Mostra.Size = new System.Drawing.Size(35, 13);
+            this.Mostra.Size = new System.Drawing.Size(42, 13);
             this.Mostra.TabIndex = 2;
-            this.Mostra.Text = "label1";
+            this.Mostra.Text = "TOTAL";
             // 
             // txtFechamento
             // 
-            this.txtFechamento.Location = new System.Drawing.Point(113, 60);
+            this.txtFechamento.Location = new System.Drawing.Point(76, 83);
             this.txtFechamento.Name = "txtFechamento";
-            this.txtFechamento.Size = new System.Drawing.Size(100, 20);
+            this.txtFechamento.ReadOnly = true;
+            this.txtFechamento.Size = new System.Drawing.Size(320, 20);
             this.txtFechamento.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FECHAMENTO DE VENDAS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 62);
+            this.panel1.TabIndex = 5;
+            // 
+            // txtDinheiro
+            // 
+            this.txtDinheiro.Location = new System.Drawing.Point(76, 120);
+            this.txtDinheiro.Name = "txtDinheiro";
+            this.txtDinheiro.Size = new System.Drawing.Size(320, 20);
+            this.txtDinheiro.TabIndex = 7;
+            this.txtDinheiro.Visible = false;
+            this.txtDinheiro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDinheiro_KeyPress);
+            // 
+            // lblDinheiro
+            // 
+            this.lblDinheiro.AutoSize = true;
+            this.lblDinheiro.Location = new System.Drawing.Point(12, 123);
+            this.lblDinheiro.Name = "lblDinheiro";
+            this.lblDinheiro.Size = new System.Drawing.Size(60, 13);
+            this.lblDinheiro.TabIndex = 6;
+            this.lblDinheiro.Text = "DINHEIRO";
+            this.lblDinheiro.Visible = false;
+            // 
+            // txtTroco
+            // 
+            this.txtTroco.Location = new System.Drawing.Point(76, 155);
+            this.txtTroco.Name = "txtTroco";
+            this.txtTroco.ReadOnly = true;
+            this.txtTroco.Size = new System.Drawing.Size(320, 20);
+            this.txtTroco.TabIndex = 9;
+            this.txtTroco.Visible = false;
+            // 
+            // lblTroco
+            // 
+            this.lblTroco.AutoSize = true;
+            this.lblTroco.Location = new System.Drawing.Point(12, 158);
+            this.lblTroco.Name = "lblTroco";
+            this.lblTroco.Size = new System.Drawing.Size(45, 13);
+            this.lblTroco.TabIndex = 8;
+            this.lblTroco.Text = "TROCO";
+            this.lblTroco.Visible = false;
+            // 
+            // gbFormaPag
+            // 
+            this.gbFormaPag.Controls.Add(this.rbPIX);
+            this.gbFormaPag.Controls.Add(this.rbCredito);
+            this.gbFormaPag.Controls.Add(this.rbDebito);
+            this.gbFormaPag.Controls.Add(this.rbDinheiro);
+            this.gbFormaPag.Dock = System.Windows.Forms.DockStyle.Right;
+            this.gbFormaPag.Location = new System.Drawing.Point(650, 62);
+            this.gbFormaPag.Name = "gbFormaPag";
+            this.gbFormaPag.Size = new System.Drawing.Size(150, 288);
+            this.gbFormaPag.TabIndex = 10;
+            this.gbFormaPag.TabStop = false;
+            this.gbFormaPag.Text = "Forma de pagamento";
+            // 
+            // rbPIX
+            // 
+            this.rbPIX.AutoSize = true;
+            this.rbPIX.Location = new System.Drawing.Point(6, 89);
+            this.rbPIX.Name = "rbPIX";
+            this.rbPIX.Size = new System.Drawing.Size(42, 17);
+            this.rbPIX.TabIndex = 3;
+            this.rbPIX.TabStop = true;
+            this.rbPIX.Text = "PIX";
+            this.rbPIX.UseVisualStyleBackColor = true;
+            this.rbPIX.CheckedChanged += new System.EventHandler(this.rbPIX_CheckedChanged);
+            // 
+            // rbCredito
+            // 
+            this.rbCredito.AutoSize = true;
+            this.rbCredito.Location = new System.Drawing.Point(6, 66);
+            this.rbCredito.Name = "rbCredito";
+            this.rbCredito.Size = new System.Drawing.Size(58, 17);
+            this.rbCredito.TabIndex = 2;
+            this.rbCredito.TabStop = true;
+            this.rbCredito.Text = "Crédito";
+            this.rbCredito.UseVisualStyleBackColor = true;
+            this.rbCredito.CheckedChanged += new System.EventHandler(this.rbCredito_CheckedChanged);
+            // 
+            // rbDebito
+            // 
+            this.rbDebito.AutoSize = true;
+            this.rbDebito.Location = new System.Drawing.Point(7, 43);
+            this.rbDebito.Name = "rbDebito";
+            this.rbDebito.Size = new System.Drawing.Size(56, 17);
+            this.rbDebito.TabIndex = 1;
+            this.rbDebito.TabStop = true;
+            this.rbDebito.Text = "Débito";
+            this.rbDebito.UseVisualStyleBackColor = true;
+            this.rbDebito.CheckedChanged += new System.EventHandler(this.rbDebito_CheckedChanged);
+            // 
+            // rbDinheiro
+            // 
+            this.rbDinheiro.AutoSize = true;
+            this.rbDinheiro.Location = new System.Drawing.Point(7, 20);
+            this.rbDinheiro.Name = "rbDinheiro";
+            this.rbDinheiro.Size = new System.Drawing.Size(64, 17);
+            this.rbDinheiro.TabIndex = 0;
+            this.rbDinheiro.TabStop = true;
+            this.rbDinheiro.Text = "Dinheiro";
+            this.rbDinheiro.UseVisualStyleBackColor = true;
+            this.rbDinheiro.CheckedChanged += new System.EventHandler(this.rbDinheiro_CheckedChanged);
             // 
             // Fechamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbFormaPag);
+            this.Controls.Add(this.txtTroco);
+            this.Controls.Add(this.lblTroco);
+            this.Controls.Add(this.txtDinheiro);
+            this.Controls.Add(this.lblDinheiro);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.txtFechamento);
             this.Controls.Add(this.Mostra);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -104,9 +248,11 @@ namespace Vendas
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Fechamento";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Load += new System.EventHandler(this.Fechamento_Load);
-            this.Shown += new System.EventHandler(this.Fechamento_Shown);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.gbFormaPag.ResumeLayout(false);
+            this.gbFormaPag.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,8 +262,19 @@ namespace Vendas
 
         private System.Windows.Forms.Button CANCELAR;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button FINALIZAR;
+        private System.Windows.Forms.Button btnFinalizar;
         private System.Windows.Forms.Label Mostra;
         public System.Windows.Forms.TextBox txtFechamento;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.TextBox txtDinheiro;
+        private System.Windows.Forms.Label lblDinheiro;
+        public System.Windows.Forms.TextBox txtTroco;
+        private System.Windows.Forms.Label lblTroco;
+        private System.Windows.Forms.GroupBox gbFormaPag;
+        private System.Windows.Forms.RadioButton rbPIX;
+        private System.Windows.Forms.RadioButton rbCredito;
+        private System.Windows.Forms.RadioButton rbDebito;
+        private System.Windows.Forms.RadioButton rbDinheiro;
     }
 }
--- Vendas Desktop/Vendas/Vendas/Telas/Fechamento.cs
+++ Vendas Desktop/Vendas/Vendas/Telas/Fechamento.cs
@@ -7,52 +7,401 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Vendas
 {
 
     public partial class Fechamento : Form
     {
-        public  int comanda;
+        NpgsqlConnection conexao = new NpgsqlConnection("Host=localhost;Database=Top11;Username=postgres;Password=Matematicas2");
 
-       
+        public int comanda;
+
+        
         public Fechamento()
         {
             InitializeComponent();
         }
 
-        
+        public Fechamento(string valor, int cNota)
+        {
+            InitializeComponent();
+            txtFechamento.Text = valor;
+            comanda = cNota;
+        }
+
+
         private void FINALIZAR_Click(object sender, EventArgs e)
         {
-            Passagem passagem = new Passagem();
 
+            if (rbDebito.Checked)
+            {
+               
+                try
+                {
 
-            // comanda = passagem.Os;
+                    conexao.Open();
+                    NpgsqlCommand comando = new NpgsqlCommand();
+                    comando.Connection = conexao;
+                    comando.CommandText = "UPDATE pedidos SET pedidos_status = 'ABERTO', pedidos_id_form_pgt = '2' WHERE pedidos_id = " + comanda + ";";
+                    comando.ExecuteNonQuery();
 
-            Mostra.Text = Convert.ToString(passagem.GetOs);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao salvar venda", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conexao.Close();
+
+                    Vendas vendas = new Vendas();
+
+                    vendas.Show();
+
+                    this.Close();
+                }
+
+            }
+            else if (rbCredito.Checked)
+            {
+                try
+                {
+
+                    conexao.Open();
+                    NpgsqlCommand comando = new NpgsqlCommand();
+                    comando.Connection = conexao;
+                    comando.CommandText = "UPDATE pedidos SET pedidos_status = 'ABERTO', pedidos_id_form_pgt = '1' WHERE pedidos_id = " + comanda + ";";
+                    comando.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao salvar venda", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conexao.Close();
+
+                    Vendas vendas = new Vendas();
+
+                    vendas.Show();
+
+                    this.Close();
+                }
+            }
+            else if (rbDinheiro.Checked)
+            {
+                float valor = float.Parse(txtTroco.Text);
+                string troco = valor.ToString().Replace(',', '.');
+
+                try
+                {
+
+                    conexao.Open();
+                    NpgsqlCommand comando = new NpgsqlCommand();
+                    comando.Connection = conexao;
+                    comando.CommandText = "UPDATE pedidos SET pedidos_status = 'ABERTO', pedidos_troco = '" + troco + "', pedidos_id_form_pgt = '3' WHERE pedidos_id = " + comanda + ";";
+                    comando.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao salvar venda", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conexao.Close();
+
+                    Vendas vendas = new Vendas();
+
+                    vendas.Show();
+                    this.Close();
+                }
+            }
+            else if (rbPIX.Checked)
+            {
+                try
+                {
+
+                    conexao.Open();
+                    NpgsqlCommand comando = new NpgsqlCommand();
+                    comando.Connection = conexao;
+                    comando.CommandText = "UPDATE pedidos SET pedidos_status = 'ABERTO', pedidos_id_form_pgt = '4' WHERE pedidos_id = " + comanda + ";";
+                    comando.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao salvar venda", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conexao.Close();
+
+                    Vendas vendas = new Vendas();
+
+                    vendas.Show();
+
+                    this.Close();
+                }
+            }
+
+            
         }
 
         private void CANCELAR_Click(object sender, EventArgs e)
         {
             Vendas vendas = new Vendas();
-           
+
+            vendas.Show();
+
             this.Close();
         }
 
-        private void Fechamento_Shown(object sender, EventArgs e)
+        private void txtDinheiro_KeyPress(object sender, KeyPressEventArgs e)
         {
-           
+            if (e.KeyChar == 13)
+            {
+               Calcula(null, null);
+
+                
+                MessageBox.Show("Troco: " + txtTroco.Text + "","Troco", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                Task.Delay(1000);
+
+                FINALIZAR_Click(null, null);
+            }
         }
 
-        private void Fechamento_Load(object sender, EventArgs e)
+        private void Calcula(object sender, EventArgs e)
         {
-            
-            Passagem passagem = new Passagem();
+            decimal dinheiro, fechamento, total;
+            dinheiro = Convert.ToDecimal(txtDinheiro.Text);
+            fechamento = Convert.ToDecimal(txtFechamento.Text);
+            total = dinheiro - fechamento;
+            txtTroco.Text = Convert.ToString(total);
+        }
 
+        private void rbDebito_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbDebito.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
 
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
 
-            Mostra.Text = passagem.GetOs.ToString();
-            MessageBox.Show(passagem.GetOs.ToString());
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbCredito.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbDinheiro.Checked)
+            {
+                txtDinheiro.Visible = true;
+                txtTroco.Visible = true;
+
+                lblDinheiro.Visible = true;
+                lblTroco.Visible = true;
+
+                btnFinalizar.Visible = false;
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbPIX.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+        }
+
+        private void rbCredito_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbDebito.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbCredito.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbDinheiro.Checked)
+            {
+                txtDinheiro.Visible = true;
+                txtTroco.Visible = true;
+
+                lblDinheiro.Visible = true;
+                lblTroco.Visible = true;
+
+                btnFinalizar.Visible = false;
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbPIX.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+        }
+
+        private void rbPIX_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbDebito.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbCredito.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbDinheiro.Checked)
+            {
+                txtDinheiro.Visible = true;
+                txtTroco.Visible = true;
+
+                lblDinheiro.Visible = true;
+                lblTroco.Visible = true;
+
+                btnFinalizar.Visible = false;
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbPIX.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+        }
+
+        private void rbDinheiro_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbDebito.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbCredito.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbDinheiro.Checked)
+            {
+                txtDinheiro.Visible = true;
+                txtTroco.Visible = true;
+
+                lblDinheiro.Visible = true;
+                lblTroco.Visible = true;
+
+                btnFinalizar.Visible = false;
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
+            else if (rbPIX.Checked)
+            {
+                txtDinheiro.Visible = false;
+                txtTroco.Visible = false;
+
+                lblDinheiro.Visible = false;
+                lblTroco.Visible = false;
+
+                btnFinalizar.Visible = true;
+
+                txtDinheiro.Text = "";
+                txtTroco.Text = "";
+            }
         }
     }
 }
